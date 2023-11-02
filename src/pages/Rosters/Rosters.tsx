b66c6b6f9ec29d184b2c8e1ebdc8c9409b3fd086
Claudia Cuevas <omordn@gmail.com>
2023-11-02 09:56:32 -0700

    add a <Rosters /> component

---
+++ src/pages/Rosters/Rosters.tsx
@@ -0,0 +1,12 @@
+import { Container, Typography } from '@mui/material'
+import * as React from 'react'
+
+const Rosters = () => {
+  return (
+    <Container sx={{ marginTop: '20%', textAlign: 'center' }}>
+      <Typography variant="h4">Coming Soon!</Typography>
+    </Container>
+  )
+}
+
+export default Rosters
