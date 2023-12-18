c3baa8b84b383891ba4c0d2f631f24b59d51c8a1
Claudia Cuevas <omordn@gmail.com>
2023-12-18 15:31:49 -0800

    use ENV variable for API

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -8,7 +8,7 @@ export const createActionWithPayload = <T>(action: string) =>
   createAction(action, withPayloadType<T>())
 
 // TODO: Update value when server is hosted
-export const API_URL = 'http://localhost:8000/'
+export const API_URL = import.meta.env.VITE_API_BASE_URL
 
 export function isObjKey<T extends object>(key: PropertyKey, obj: T): key is keyof T {
   return key in obj
