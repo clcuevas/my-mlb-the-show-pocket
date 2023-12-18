fda4c243c2bb845b34394b5e4f2e961537d968fa
Claudia Cuevas <omordn@gmail.com>
2023-12-18 15:23:21 -0800

    update localhost PORT

--- src/services/helpers.ts
+++ src/services/helpers.ts
@@ -8,7 +8,7 @@ export const createActionWithPayload = <T>(action: string) =>
   createAction(action, withPayloadType<T>())
 
 // TODO: Update value when server is hosted
-export const API_URL = 'http://localhost:4000/'
+export const API_URL = 'http://localhost:8000/'
 
 export function isObjKey<T extends object>(key: PropertyKey, obj: T): key is keyof T {
   return key in obj
